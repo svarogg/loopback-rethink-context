4709f4d144f9422454cfa00a9facf7e61c8f05ee
Mike Zak <mike@perfectedtech.com>
2015-06-30 14:00:34 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
+
+app.use(loopback.context());
+app.use(loopback.token());
+
+app.use(function (req, res, next) {
+  var loopbackContext = loopback.getCurrentContext();
+  console.log("Context in server.js: ", loopbackContext);
+  next();
+});
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
